530715e1ff373e2ffa83a4a70510cbda82b729ea
Dudin Sergey <noisy44@gmail.com>
2025-05-10 12:12:00 +0300

    Создание скрипта доработка 4

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ DOMAIN="ya.ru"
 COUNT=1
 
 if ping -c $COUNT $DOMAIN > /dev/null 2>&1; then
-    echo "Подключение к $DOMAIN успешно установлено"
+    echo "Подключение  к  $DOMAIN успешно установлено"
 else
     echo "Ошибка подключения к $DOMAIN"
 fi
