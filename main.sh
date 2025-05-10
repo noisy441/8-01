24b58f3ad5575fbcd8c2bdceabb8bd46eaed7872
Dudin Sergey <noisy44@gmail.com>
2025-05-10 12:14:44 +0300

    Создание скрипта доработка 3

--- main.sh
+++ main.sh
@@ -5,7 +5,6 @@ DOMAIN="netology.ru"
 # Количество попыток пинга
 COUNT=1
 
-# Выполняем пинг и проверяем результат
 if ping -c $COUNT $DOMAIN > /dev/null 2>&1; then
     echo "Подключение к $DOMAIN успешно установлено"
 else
