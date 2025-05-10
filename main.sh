ac9affb6e71834d77bf8f48c26ebb89d6384c42a
Dudin Sergey <noisy44@gmail.com>
2025-05-10 12:14:25 +0300

    Создание скрипта доработка 2

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Указываем домен Яндекса
+DOMAIN="netology.ru"
+# Количество попыток пинга
+COUNT=1
+
+# Выполняем пинг и проверяем результат
+if ping -c $COUNT $DOMAIN > /dev/null 2>&1; then
+    echo "Подключение к $DOMAIN успешно установлено"
+else
+    echo "Ошибка подключения к $DOMAIN"
+fi
+
